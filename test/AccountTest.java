106db38cca795bdabf99dd02260745a3c7563b01
RevEmmanuel <adeolaae1@gmail.com>
2022-11-25 13:28:51 +0100

    upda

--- test/AccountTest.java
+++ test/AccountTest.java
@@ -3,20 +3,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AccountTest {
-    Account account;
-
-    @BeforeEach
-    void setUp() {
-        account = new Account("Amila");
-    }
 
     @Test
     void getName() {
+        Account account = new Account("Amila");
         assertEquals("Amila", account.getName());
     }
 
     @Test
     void setName() {
+        Account account = new Account("Amila");
         account.setName("Amala");
         assertEquals("Amala", account.getName());
     }
