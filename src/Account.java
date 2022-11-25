3895061e0d29d33baad5dc966c75fcaa146ea0fb
RevEmmanuel <adeolaae1@gmail.com>
2022-11-25 13:30:46 +0100

    Test completed

--- src/Account.java
+++ src/Account.java
@@ -1,9 +1,19 @@
 public class Account {
 
     private String name;
+    private double balance;
 
-    public Account(String name) {
+    public Account(String name, double balance) {
         this.name = name;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
     public String getName() {
